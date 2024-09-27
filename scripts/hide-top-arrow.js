369d56daed41ea7a9bb4df903cb3c060627c4937
JL-Star-B <151557611+JL-Star-B@users.noreply.github.com>
2024-09-27 00:44:38 -0400

    updated/improved home page

--- scripts/hide-top-arrow.js
+++ scripts/hide-top-arrow.js
@@ -12,4 +12,15 @@ function scrollFunction()
 		document.getElementById("top-arrow").style.display = "none";
 		document.getElementById("top-arrow").style.opacity = "0";
 	}
+
+	// if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
+	// {
+	// 	document.getElementById("top-arrow").style.animation = "arrow-slide-in";
+	// 	// document.getElementById("top-arrow").style.animationPlayState = "running";
+	// }
+	// else
+	// {
+	// 	document.getElementById("top-arrow").style.animation = "arrow-slide-out";
+	// 	// document.getElementById("top-arrow").style.animationPlayState = "running";
+	// }
 }
